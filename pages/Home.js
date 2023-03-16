6a8bc62a3ef8ff1f5aec8d59cd9343b1f4f61691
tog22 <tom.ash.github@philosofiles.com>
2023-03-16 15:56:24 -0700

    Routing: part 2, convert to createNativeStackNavigator

---
+++ pages/Home.js
@@ -0,0 +1,10 @@
+/////  External libraries  /////
+import { View, Text } from 'react-native';
+
+export default function Home() {
+	return (
+		<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+			<Text>Home Screen</Text>
+		</View>
+	);
+}
