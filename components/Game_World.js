f42160e2621a65932d8cffb61a4464633af73fed
tog22 <tom.ash.github@philosofiles.com>
2023-03-21 16:08:32 -0700

    *HOWTO* Set up CSS modules, demo different CSS styling methods

--- components/Game_World.js
+++ components/Game_World.js
@@ -1,6 +1,6 @@
 /////  External libraries  /////
 import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 
 
 /////  Components  /////
@@ -8,6 +8,7 @@ import { View, Text } from 'react-native';
 
 /////  Other  /////
 import starting_sotw from '../auxiliary/SOTW_Start'
+import style from '../App.module.css'
 
 export default function Game_World({online_screen}) {
 	
@@ -27,8 +28,17 @@ export default function Game_World({online_screen}) {
 
 
 	return (
-		<View>
-			<Text>Game World</Text>
+		<View style="style.game_world">
+			<Text style={{color: 'red'}}>This element is wrapped in "&#x3C;Text style=color: &#x27;red&#x27;&#x22;&#x3E;"</Text>
+			<Text style={style.game_world}>This element is wrapped in &#x3C;Text style=style.game_world&#x3E;&#x3C;/Text</Text>
+			<Text style={stylesheet_create.red}>This element is wrapped in &#x3C;Text style=stylesheet_create.red&#x3E;&#x3C;/Text&#x3E;</Text>
 		</View>
 	);
 }
+
+const stylesheet_create = StyleSheet.create({
+    red: {
+        color: 'green',
+        backgroundColor: "red",
+    }
+})	
